a890c4aada6106e147b9ba10b6621606a7c89902
brightdevs <38365682+e-rojas@users.noreply.github.com>
2022-09-18 13:09:07 -0300

    rendering template

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log('Hello World');
+class projectInput {
+  templateElement: HTMLTemplateElement;
+  renderElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById('project-input')!
+    );
+    this.renderElement = <HTMLDivElement>document.getElementById('app')!;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+
+    /* --- */
+    this.attach();
+  }
+
+  private attach() {
+    this.renderElement.insertAdjacentElement('afterbegin', this.formElement);
+  }
+}
+
+const project = new projectInput();
